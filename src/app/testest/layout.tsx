d5bc8873043fa7482fb9e2e6dde4a36ce8d893ae
rs-taehun <startup@rsteam.com>
2023-05-08 03:29:09 +0900

    레이아웃 링크 추가

--- src/app/testest/layout.tsx
+++ src/app/testest/layout.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Hero({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="bg-gray-100 dark:bg-gray-900 py-4">
         <div className="container mx-auto px-3">
           <h2 className="text-4xl font-bold text-center text-gray-800 dark:text-white">
-            테스트들
+            <Link href={"/testest"}>테스트들</Link>
           </h2>
         </div>
       </div>
