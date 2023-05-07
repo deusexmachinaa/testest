0580278b6f00f3da948c8b0b7ffe0c459327421e
rs-taehun <startup@rsteam.com>
2023-05-08 04:57:17 +0900

    rewrite 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/versus/:path*",
+        destination: "/versus/:path*/result",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
