c1ef36528a58d64e45e43862c9be57fca71a4d70
rs-taehun <startup@rsteam.com>
2023-05-10 09:44:12 +0900

    Utterances init

--- src/components/Utterances.tsx
+++ src/components/Utterances.tsx
@@ -8,9 +8,9 @@ function Utterances() {
         const scriptElement = document.createElement('script');
         scriptElement.src = 'https://utteranc.es/client.js';
         scriptElement.async = true;
-        scriptElement.setAttribute('repo', '본인의 utterances 레포지토리');
-        scriptElement.setAttribute('issue-term', '댓글을 어떻게 관리할 것인지');
-        scriptElement.setAttribute('theme', '원하는 테마');
+        scriptElement.setAttribute('repo', 'deusexmachinaa/Utterances');
+        scriptElement.setAttribute('issue-term', 'pathname');
+        scriptElement.setAttribute('theme', 'preferred-color-scheme');
         scriptElement.setAttribute('crossorigin', 'anonymous');
         elem.appendChild(scriptElement);
       }}
