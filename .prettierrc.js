400f940ec8ac48818ba3ad85e7e88c89ab3d2ba6
rs-taehun <startup@rsteam.com>
2023-05-12 22:00:18 +0900

    prettier 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 100,
-    semi: true,
-    tab
-}
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+};
